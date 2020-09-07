94f8d70822bfbc67fff32bc8a138ea1ee8aaf95f
Paul Wienk <paulwienk@gmail.com>
2020-09-07 15:31:54 +0200

    fsjidofsbs

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,7 @@
+import os
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("kick.wav")
+print(f'{os.getcwd()}/{__file__}')
+wave_obj = sa.WaveObject.from_wave_file("D:\hku\jaar2\CSD2\python_basics\kick.wav")
 play_obj = wave_obj.play()
 play_obj.wait_done()
