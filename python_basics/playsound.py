1b576924179bde534dbbf2e524ac239d39a6e0aa
Paul Wienk <paulwienk@gmail.com>
2020-09-07 22:21:18 +0200

    fixed user input

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,8 +1,24 @@
-import os
-import simpleaudio as sa
-path = os.path.basename(__file__)
-path = __file__.replace(path, 'kick.wav')
+from pydub import AudioSegment
+from pydub.playback import play
 
-wave_obj = sa.WaveObject.from_wave_file(path)
-play_obj = wave_obj.play()
-play_obj.wait_done()
+file_name = "kick.wav"
+
+# ask for the file name (has to be in the right folder)
+file_text = input ("What file would you like to play: ")
+file_name = str(file_text)
+
+
+# asks for the number of times the file is being played
+try: 
+    test_text = input ("How many times would you like to hear the audio: ")
+    test_number = int(test_text)
+
+except ValueError:
+    print("This isn't a number.")
+
+n = test_number
+
+# plays audio the exact number of times you gave as the input
+audio = AudioSegment.from_file(file_name)
+play(audio * n) 
+
