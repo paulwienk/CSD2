407d612b4ed79afe0a8393e3f9c9ea059d5e08a9
Paul Wienk <paulwienk@gmail.com>
2021-04-26 19:11:20 +0200

    added specific frequency to color

--- CSD2d/playful_music/sketch.js
+++ CSD2d/playful_music/sketch.js
@@ -3,10 +3,16 @@ const synth = new Tone.Synth().toDestination();
 
 let currentColor = 255;
 let currentThickness = 6;
+let currentFreq = 396;
 
 function setup() {
   createCanvas(1200, 600);
   background(104);
+
+  // create clear button to clear the casnvas
+  clearLines = createButton('Clear');
+  clearLines.position(0, 500);
+  clearLines.mousePressed(clearCanvas)
 
   // create buttons for thickness of the line
   smallThickness = createButton('Small');
@@ -26,13 +32,13 @@ function setup() {
   blackButton.position(0, 0);
   blackButton.mousePressed(changeColorToBlack);
 
-  redButton = createButton('Red');
-  redButton.position(0, 50);
-  redButton.mousePressed(changeColorToRed);
-
   blueButton = createButton('Blue');
-  blueButton.position(0, 100);
+  blueButton.position(0, 50);
   blueButton.mousePressed(changeColorToBlue);
+
+  redButton = createButton('Red');
+  redButton.position(0, 100);
+  redButton.mousePressed(changeColorToRed);
 
   greenButton = createButton('Green');
   greenButton.position(0, 150);
@@ -45,6 +51,11 @@ function setup() {
   whiteButton = createButton('White');
   whiteButton.position(0, 250);
   whiteButton.mousePressed(changeColorToWhite);
+}
+
+function clearCanvas() {
+  clear();
+  background(104);
 }
 
 // functions to change the thickness
@@ -60,31 +71,36 @@ function changeThicknessToLarge() {
   currentThickness = 12;
 }
 
-// functions to change the colors
+// functions to change the colors and frequency
 function changeColorToBlack() {
   currentColor = 0;
-}
-
-function changeColorToRed() {
-  currentColor = color(255, 0, 0);
+  currentFreq = 396;
 }
 
 function changeColorToBlue() {
   currentColor = color(0, 0, 255);
+  currentFreq = 417;
+}
+
+function changeColorToRed() {
+  currentColor = color(255, 0, 0);
+  currentFreq = 528;
 }
 
 function changeColorToGreen() {
   currentColor = color(0, 255, 0);
+  currentFreq = 639;
 }
 
 function changeColorToYellow() {
   currentColor = color(255, 255, 0);
+  currentFreq = 741;
 }
 
 function changeColorToWhite() {
   currentColor = 255;
+  currentFreq = 852;
 }
-
 
 function draw() {
   stroke(currentColor);
@@ -95,9 +111,7 @@ function draw() {
     console.log(mouseX);
 
     const now = Tone.now();
-    synth.triggerAttack(mouseX, now);
-
-    // wait one second before triggering the release
+    synth.triggerAttack(currentFreq, now);
     synth.triggerRelease(now + 0.0001);
   }
 
