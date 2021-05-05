e17ed4c434bb31ebc10d28b9c3b4f78a9f4d67d6
Paul Wienk <paulwienk@gmail.com>
2021-05-05 17:20:58 +0200

    added autofiltering and fixed drawing only inside canvas

--- CSD2d/playful_music/sketch.js
+++ CSD2d/playful_music/sketch.js
@@ -4,6 +4,7 @@
 // - filtering
 // - multithreading
 // - smoother adsr
+// - button clicking
 
 let currentColor = 0;
 let currentThickness = 6;
@@ -11,60 +12,76 @@ let currentFreq = 396;
 let mouseReleased = false;
 let lengthLine = 0;
 
-let adsr = new Tone.AmplitudeEnvelope({
+let drawingCanvasWidth = 1000;
+let drawingCanvasHeight = 600;
+let drawingCanvasStartX = 80;
+let drawingCanvasStartY = 0;
+
+
+const adsr = new Tone.AmplitudeEnvelope({
 "attack": 2.0,
 "decay": 0.2,
 "sustain": 0.8,
-"release": 2.0
-}).toMaster();
+"release": 10.0
+}).toDestination();
 
-var oscillator = new Tone.Oscillator(currentFreq, "sine").connect(adsr).start();
+const filter = new Tone.Filter(350, "lowpass").toDestination();
+
+const autoFilter = new Tone.AutoFilter("8n").toDestination().start();
+
+const oscillator = new Tone.Oscillator(currentFreq, "sine")
+
+oscillator.chain(adsr, filter, autoFilter, Tone.Destination);
+
 
 function setup()
 {
   createCanvas(1200, 600);
-  background(104);
+  background(30);
+
+  fill(104);
+  rect(drawingCanvasStartX, drawingCanvasStartY,
+       drawingCanvasWidth, drawingCanvasHeight);
 
   // create clear button to clear the casnvas
   clearLines = createButton('Clear');
-  clearLines.position(0, 500);
+  clearLines.position(10, 500);
   clearLines.mousePressed(clearCanvas)
 
-  // create buttons for thickness of the line
   smallThickness = createButton('Small');
-  smallThickness.position(1135, 0);
+  smallThickness.position(1100, 0);
   smallThickness.mousePressed(changeThicknessToSmall)
 
   mediumThickness = createButton('Medium');
-  mediumThickness.position(1135, 50);
+  mediumThickness.position(1100, 50);
   mediumThickness.mousePressed(changeThicknessToMedium)
 
   largeThickness = createButton('Large');
-  largeThickness.position(1135, 100);
+  largeThickness.position(1100, 100);
   largeThickness.mousePressed(changeThicknessToLarge)
 
   blackButton = createButton('Black');
-  blackButton.position(0, 0);
+  blackButton.position(10, 0);
   blackButton.mousePressed(changeColorToBlack);
 
   blueButton = createButton('Blue');
-  blueButton.position(0, 50);
+  blueButton.position(10, 50);
   blueButton.mousePressed(changeColorToBlue);
 
   redButton = createButton('Red');
-  redButton.position(0, 100);
+  redButton.position(10, 100);
   redButton.mousePressed(changeColorToRed);
 
   greenButton = createButton('Green');
-  greenButton.position(0, 150);
+  greenButton.position(10, 150);
   greenButton.mousePressed(changeColorToGreen);
 
   yellowButton = createButton('Yellow');
-  yellowButton.position(0, 200);
+  yellowButton.position(10, 200);
   yellowButton.mousePressed(changeColorToYellow);
 
   whiteButton = createButton('White');
-  whiteButton.position(0, 250);
+  whiteButton.position(10, 250);
   whiteButton.mousePressed(changeColorToWhite);
 }
 
@@ -135,7 +152,11 @@ function draw()
   stroke(currentColor);
   strokeWeight(currentThickness);
 
-  if (mouseIsPressed === true)
+  if ((mouseIsPressed === true) &&
+      (mouseX > drawingCanvasStartX) &&
+      (mouseX < drawingCanvasStartX + drawingCanvasWidth) &&
+      (mouseY > drawingCanvasStartY) &&
+      (mouseY < drawingCanvasStartY + drawingCanvasHeight))
   {
     line(mouseX, mouseY, pmouseX, pmouseY);
     lengthLine++;
@@ -143,7 +164,9 @@ function draw()
 
   if (mouseReleased === true)
   {
-    let lengthLineMax = max(lengthLine) / 20;
+    var lengthLineMax = max(lengthLine) / 20;
+
+    oscillator.start();
 
 		oscillator.frequency.value = currentFreq;
 		adsr.triggerAttackRelease(lengthLineMax);
