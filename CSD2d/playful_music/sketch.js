5a3d57389da59e0bdfd2cedad1b83239953ac042
Paul Wienk <paulwienk@gmail.com>
2021-05-11 17:40:25 +0200

    fixed polyphony

--- CSD2d/playful_music/sketch.js
+++ CSD2d/playful_music/sketch.js
@@ -2,23 +2,61 @@
 
 // TO DO:
 // - filtering
-// - multithreading
 // - smoother adsr
-// - button clicking
 
 let currentLineColor = 0;
 let currentLineThickness = 6;
 let mouseReleased = false;
 let lengthLine = 0;
+
+let colorIsBlack = true;
 let colorIsBlue = false;
-let colorIsBlack = false;
+let colorIsRed = false;
+let colorIsGreen = false;
+let colorIsYellow = false;
+let colorIsWhite = false;
 
 let drawingCanvasWidth = 1000;
 let drawingCanvasHeight = 600;
 let drawingCanvasTopLeftX = 80;
 let drawingCanvasTopLeftY = 0;
 
-const adsr = new Tone.AmplitudeEnvelope({
+const adsrBlack = new Tone.AmplitudeEnvelope({
+"attack": 2.0,
+"decay": 0.2,
+"sustain": 0.8,
+"release": 10.0
+}).toDestination();
+
+const adsrBlue = new Tone.AmplitudeEnvelope({
+"attack": 2.0,
+"decay": 0.2,
+"sustain": 0.8,
+"release": 10.0
+}).toDestination();
+
+const adsrRed = new Tone.AmplitudeEnvelope({
+"attack": 2.0,
+"decay": 0.2,
+"sustain": 0.8,
+"release": 10.0
+}).toDestination();
+
+const adsrGreen = new Tone.AmplitudeEnvelope({
+"attack": 2.0,
+"decay": 0.2,
+"sustain": 0.8,
+"release": 10.0
+}).toDestination();
+
+const adsrYellow = new Tone.AmplitudeEnvelope({
+"attack": 2.0,
+"decay": 0.2,
+"sustain": 0.8,
+"release": 10.0
+}).toDestination();
+
+const adsrWhite = new Tone.AmplitudeEnvelope({
 "attack": 2.0,
 "decay": 0.2,
 "sustain": 0.8,
@@ -36,12 +74,12 @@ const oscillatorGreen = new Tone.Oscillator(639, "sine")
 const oscillatorYellow = new Tone.Oscillator(741, "sine")
 const oscillatorWhite = new Tone.Oscillator(852, "sine")
 
-oscillatorBlack.chain(adsr, filter, autoFilter, Tone.Destination);
-oscillatorBlue.chain(adsr, filter, autoFilter, Tone.Destination);
-oscillatorRed.chain(adsr, filter, autoFilter, Tone.Destination);
-oscillatorGreen.chain(adsr, filter, autoFilter, Tone.Destination);
-oscillatorYellow.chain(adsr, filter, autoFilter, Tone.Destination);
-oscillatorWhite.chain(adsr, filter, autoFilter, Tone.Destination);
+oscillatorBlack.chain(adsrBlack, filter, autoFilter, Tone.Destination);
+oscillatorBlue.chain(adsrBlue, filter, autoFilter, Tone.Destination);
+oscillatorRed.chain(adsrRed, filter, autoFilter, Tone.Destination);
+oscillatorGreen.chain(adsrGreen, filter, autoFilter, Tone.Destination);
+oscillatorYellow.chain(adsrYellow, filter, autoFilter, Tone.Destination);
+oscillatorWhite.chain(adsrWhite, filter, autoFilter, Tone.Destination);
 
 function setup()
 {
@@ -123,32 +161,66 @@ function changeColorToBlack()
 {
   currentLineColor = 0;
   colorIsBlack = true;
+  colorIsBlue = false;
+  colorIsRed = false;
+  colorIsGreen = false;
+  colorIsYellow = false;
+  colorIsWhite = false;
 }
 
 function changeColorToBlue()
 {
   currentLineColor = color(0, 0, 255);
   colorIsBlue = true;
+  colorIsBlack = false;
+  colorIsRed = false;
+  colorIsGreen = false;
+  colorIsYellow = false;
+  colorIsWhite = false;
 }
 
 function changeColorToRed()
 {
   currentLineColor = color(255, 0, 0);
+  colorIsRed = true;
+  colorIsBlack = false;
+  colorIsBlue = false;
+  colorIsGreen = false;
+  colorIsYellow = false;
+  colorIsWhite = false;
 }
 
 function changeColorToGreen()
 {
   currentLineColor = color(0, 255, 0);
+  colorIsGreen = true;
+  colorIsBlack = false;
+  colorIsBlue = false;
+  colorIsRed = false;
+  colorIsYellow = false;
+  colorIsWhite = false;
 }
 
 function changeColorToYellow()
 {
   currentLineColor = color(255, 255, 0);
+  colorIsYellow = true;
+  colorIsBlack = false;
+  colorIsBlue = false;
+  colorIsGreen = false;
+  colorIsRed = false;
+  colorIsWhite = false;
 }
 
 function changeColorToWhite()
 {
   currentLineColor = 255;
+  colorIsWhite = true;
+  colorIsBlack = false;
+  colorIsBlue = false;
+  colorIsRed = false;
+  colorIsGreen = false;
+  colorIsYellow = false;
 }
 
 function mouseClicked()
@@ -185,19 +257,42 @@ function draw()
     if (colorIsBlack === true)
     {
     oscillatorBlack.start();
-    colorIsBlack = false;
+    adsrBlack.triggerAttackRelease(lengthLineMax);
     }
 
     if (colorIsBlue === true)
     {
     oscillatorBlue.start();
-    colorIsBlue = false;
+    adsrBlue.triggerAttackRelease(lengthLineMax);
     }
 
-		adsr.triggerAttackRelease(lengthLineMax);
+    if (colorIsRed === true)
+    {
+    oscillatorRed.start();
+    adsrRed.triggerAttackRelease(lengthLineMax);
+    }
+
+    if (colorIsGreen === true)
+    {
+    oscillatorGreen.start();
+    adsrGreen.triggerAttackRelease(lengthLineMax);
+    }
+
+    if (colorIsYellow === true)
+    {
+    oscillatorYellow.start();
+    adsrYellow.triggerAttackRelease(lengthLineMax);
+    }
+
+    if (colorIsWhite === true)
+    {
+    oscillatorWhite.start();
+    adsrWhite.triggerAttackRelease(lengthLineMax);
+    }
 
     lengthLine = 0;
     mouseReleased = false;
     console.log("start release", lengthLineMax, colorIsBlack);
   }
+  console.log("black = ", colorIsBlack, "white = ", colorIsWhite)
 }
