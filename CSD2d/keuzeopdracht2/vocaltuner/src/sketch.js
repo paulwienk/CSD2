c250529dbe31c075bb060b7ca933a59e8142757c
Paul Wienk <paulwienk@gmail.com>
2021-05-31 15:39:43 +0200

    fixed pitch detection and drawing a pitch line and goal line

--- CSD2d/keuzeopdracht2/vocaltuner/src/sketch.js
+++ CSD2d/keuzeopdracht2/vocaltuner/src/sketch.js
@@ -1,28 +1,106 @@
-let mic;
-let threshold = 0.2;
+// template by Daniel Shiffman, adjusted by Paul Wienk
+
+const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+let pitch, mic;
+let freq = 0;
+let canvasWidth = 360;
+let canvasHeigth = 640;
+let randomNote, goalNote, goalFrequency;
+
+let notes = [
+  {
+    note: 'A',
+    frequency: 440.0
+  },
+  {
+    note: 'B',
+    frequency: 493.9
+  },
+  {
+    note: 'C',
+    frequency: 261.6
+  },
+  {
+    note: 'D',
+    frequency: 293.7
+  },
+  {
+    note: 'E',
+    frequency: 329.6
+  },
+  {
+    note: 'F',
+    frequency: 349.2
+  },
+  {
+    note: 'G',
+    frequency: 392.0
+  },
+];
 
 function setup()
 {
-  createCanvas(300, 400)
-  setupAudio()
+  createCanvas(canvasWidth, canvasHeigth);
+  audioContext = getAudioContext();
+  mic = new p5.AudioIn();
+  mic.start(detectPitch);
+
+  randomNote = int(random(notes.length - 1));
+  goalNote = notes[randomNote].note
+  goalFrequency = notes[randomNote].frequency
 }
 
-function draw(){
-  background(0);
+function detectPitch()
+{
+  console.log('listening');
+  pitch = ml5.pitchDetection(
+    model_url,
+    audioContext,
+    mic.stream,
+    modelLoaded
+  );
+}
+
+function createGoalLine()
+{
+  line(0, goalFrequency + 10, canvasWidth, goalFrequency + 10)
+  line(0, goalFrequency - 10, canvasWidth, goalFrequency - 10)
+  text(goalNote, width / 2, goalFrequency - 30)
+}
+
+function draw()
+{
+  background(130);
+  textAlign(CENTER, CENTER);
   fill(255);
+  textSize(32);
+  text(freq.toFixed(1), width / 2, height - 50);
 
-  amplitude = mic.getLevel() * 1000;
+  line(0, freq, canvasWidth, freq);
+  createGoalLine()
+}
 
-  if (amplitude >= threshold)
+function modelLoaded()
+{
+  console.log('model loaded');
+  pitch.getPitch(gotPitch);
+}
+
+function gotPitch(error, frequency)
+{
+  if (error)
   {
-    console.log(amplitude);
+    console.error(error);
   }
 
-}
+  else
+  {
+    //console.log(frequency);
+    if (frequency)
+    {
+      freq = frequency;
+    }
 
-function setupAudio() {
-	userStartAudio();
-	mic = new p5.AudioIn();
-	mic.start();
-  getAudioContext().resume();
+    pitch.getPitch(gotPitch);
+  }
 }
