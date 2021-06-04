3b7930e94901d017cafdd667da5ba2e8b3f6b235
Paul Wienk <paulwienk@gmail.com>
2021-06-04 14:04:45 +0200

    created some color mapping to the frequency

--- CSD2d/keuzeopdracht2/vocaltuner/src/sketch.js
+++ CSD2d/keuzeopdracht2/vocaltuner/src/sketch.js
@@ -10,6 +10,8 @@ let canvasHeigth = 640;
 let goalLineDifference = 10;
 let currentOctave = 3;
 let timer = 3;
+let currentFrequencyLineColor = [255, 30, 0];
+let insideGoalRange = false;
 
 // define notes with their frequencies
 let notes = [
@@ -43,6 +45,18 @@ let notes = [
   },
 ];
 
+// loading voice overs
+function preload()
+{
+  voiceOverA = loadSound('assets/voiceover_a.mp3');
+  voiceOverB = loadSound('assets/voiceover_b.mp3');
+  voiceOverC = loadSound('assets/voiceover_c.mp3');
+  voiceOverD = loadSound('assets/voiceover_d.mp3');
+  voiceOverE = loadSound('assets/voiceover_e.mp3');
+  voiceOverF = loadSound('assets/voiceover_f.mp3');
+  voiceOverG = loadSound('assets/voiceover_g.mp3');
+}
+
 function setup()
 {
   createCanvas(canvasWidth, canvasHeigth);
@@ -52,16 +66,25 @@ function setup()
   mic = new p5.AudioIn();
   mic.start(detectPitch);
 
+  // creating buttons
   octaveShiftUp = createButton('octave shift up');
   octaveShiftUp.position(5, 0);
   octaveShiftUp.mousePressed(shiftOctaveUp);
 
   octaveShiftDown = createButton('octave shift down');
-  octaveShiftDown.position(233, 0);
+  octaveShiftDown.position(5, 25);
   octaveShiftDown.mousePressed(shiftOctaveDown);
 
+  goalRangeClose = createButton('goal range close');
+  goalRangeClose.position(235, 25);
+  goalRangeClose.mousePressed(goalLinesClose);
+
+  goalRangeOpen = createButton('goal range open');
+  goalRangeOpen.position(235, 0);
+  goalRangeOpen.mousePressed(goalLinesOpen);
+
   newNote = createButton('new note');
-  newNote.position(5, 40);
+  newNote.position(5, 600);
   newNote.mousePressed(createNewNote);
 
   // picks a random object from the notes array
@@ -70,6 +93,8 @@ function setup()
   // takes the right note and frequency of that object
   goalNote = notes[randomNote].note
   goalFrequency = -notes[randomNote].frequency
+
+  playVoiceOver();
 }
 
 // detects the pitch based on the CREPE model
@@ -110,10 +135,10 @@ function modelLoaded()
 function createGoalLine()
 {
   // lower goal line
-  line(0, goalFrequency + goalLineDifference, canvasWidth, goalFrequency + goalLineDifference);
+  line(0, goalFrequency + goalLineDifference, canvasWidth, goalFrequency +                      goalLineDifference);
 
   // upper goal line
-  line(0, goalFrequency - goalLineDifference, canvasWidth, goalFrequency - goalLineDifference);
+  line(0, goalFrequency - goalLineDifference, canvasWidth, goalFrequency -                      goalLineDifference);
 
   // display the current note
   text(goalNote, width / 2.15, goalFrequency - 30);
@@ -122,25 +147,90 @@ function createGoalLine()
   text(currentOctave, width / 1.85, goalFrequency - 30);
 }
 
+// function to make the goal range narrower
+function goalLinesClose()
+{
+  goalLineDifference = goalLineDifference - 1;
+}
+
+// function to make the goal range bigger
+function goalLinesOpen()
+{
+  goalLineDifference = goalLineDifference + 1;
+}
+
+// function that check if the current frequency is inside the goal range
+function checksIfInsideGoalRange()
+{
+  if (-currentFrequency <= goalFrequency + goalLineDifference &&
+      -currentFrequency >= goalFrequency - goalLineDifference)
+  {
+    currentFrequencyLineColor = [0, 255, 0];
+    insideGoalRange = true;
+  }
+
+  else
+  {
+    currentFrequencyLineColor = [255, 30, 255];
+    insideGoalRange = false;
+  }
+}
+
 // checks if you were inside the goal lines for 3 seconds
 function checksRightPitch()
 {
-  if (-currentFrequency <= goalFrequency + goalLineDifference &&
-      -currentFrequency >= goalFrequency - goalLineDifference &&
-      frameCount % 60 == 0 && timer > 0)
+  if (insideGoalRange === true && frameCount % 60 == 0 && timer > 0)
   {
     timer--
-    console.log(timer);
+    currentFrequencyLineColor = [0, 255, 0];
 
     // if this happens, 3 seconds have passed and the next note starts
     if (timer === 0)
       {
-        console.log('green');
         timer = 3;
 
         // creates new note
         createNewNote();
       }
+  }
+}
+
+// function that checks the current note and plays the right voice over
+function playVoiceOver()
+{
+  if (goalNote == 'A')
+  {
+    voiceOverA.play();
+  }
+
+  else if (goalNote == 'B')
+  {
+    voiceOverB.play();
+  }
+
+  else if (goalNote == 'C')
+  {
+    voiceOverC.play();
+  }
+
+  else if (goalNote == 'D')
+  {
+    voiceOverD.play();
+  }
+
+  else if (goalNote == 'E')
+  {
+    voiceOverE.play();
+  }
+
+  else if (goalNote == 'F')
+  {
+    voiceOverF.play();
+  }
+
+  else if (goalNote == 'G')
+  {
+    voiceOverG.play();
   }
 }
 
@@ -150,6 +240,9 @@ function createNewNote()
   randomNote = int(random(notes.length - 1));
   goalNote = notes[randomNote].note
   goalFrequency = -notes[randomNote].frequency
+
+  // play voice over when the new note is created
+  playVoiceOver();
 }
 
 // shift octave one up
@@ -178,10 +271,12 @@ function draw()
   textAlign(CENTER, CENTER);
   fill(255);
   textSize(32);
+
+  // display the current frequency at the bottom
   text(currentFrequency.toFixed(1), width / 2, -30);
 
   // draw the line of the current frequency that is heard
-  stroke(0);
+  stroke(currentFrequencyLineColor);
   line(0, -currentFrequency, canvasWidth, -currentFrequency);
 
   // draw the goal line
@@ -189,4 +284,5 @@ function draw()
   createGoalLine();
 
   checksRightPitch();
+  checksIfInsideGoalRange();
 }
